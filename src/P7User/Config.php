a997504c2a2cd8e6c7e9ff46d67c4b9b3dc19159
sven@schrodt-service.net <sven@schrodt-service.net>
2020-06-10 16:23:47 +0200

    Initial commit

---
+++ src/P7User/Config.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types = 1);
+/**
+ * \P7User\Config
+ *
+ * Class for configuration
+ *
+ * - magical interceptors for READONLY access to class members
+ * - explicit setter functions for validation purposes
+ *
+ * @package P7User
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @version 0.1
+ * @since 2020-06-10
+ * @link https://github.com/svenschrodt/P7User
+ * @license https://github.com/svenschrodt/P7User/blob/master/LICENSE.md
+ * @copyright Sven Schrodt<sven@schrodt-service.net>
+ */
+namespace P7User;
+
+class Config
+{
+
+    
+    
+    /**
+     * Constructor function 
+     * 
+     * @todo optional parameters
+     */
+    public function __construct()
+    {
+        
+    }
+    
+    /**
+     * Magical interceptor for public access to class members
+     * 
+     * @param string $name
+     * @return NULL|string
+     */
+    public function __get(string $name)
+    {
+        $return = null;
+        switch(strtolower($name)) {
+            case  'adaptor' :
+                $return = $this->adaptor;
+                break;
+                
+            case  'test' :
+                $return = 'FOO 42!';
+                break;
+                
+            default:
+                $return  = (isset($this->$name)) ? $this->$name : null;
+                
+        }
+        
+        return $return;
+    }
+ 
+}
---
+++ test/P7User/BasicTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Basic testing of PHPUnit's functionality
+ * 
+ * 
+ * @link https://github.com/svenschrodt/P7User
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @package P7Tools
+ * @license https://github.com/svenschrodt/P7Tools/blob/master/LICENSE.md
+ * @copyright Sven Schrodt<sven@schrodt-service.net>
+ * @version 0.0.23
+ */
+class BasicTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testInstatiationOfFoo()
+    {
+        $foo = new \P7User\Config();
+        $this->assertInstanceOf('\P7User\Config', $foo);
+       
+    }
+    public function testInstatiationOfBar()
+    {
+        $foo = new \P7User\Config();
+        $this->assertTrue(is_object($foo));
+        
+    }
+}
+
+
